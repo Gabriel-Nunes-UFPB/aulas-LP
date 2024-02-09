d4b1333d991997c094291c9be87c3bc9da53a9ba
Kenobi <140012988+GabrielNunesGomes@users.noreply.github.com>
2024-02-09 14:20:32 -0300

    Agenda 2 e 3

---
+++ AGENDA_3/src/Main.java
@@ -0,0 +1,154 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static final int tamanhoDaAgenda = 50;
+    private static String[] nomesDaAgenda = new String[tamanhoDaAgenda];
+    private static String[] numerosDaAgenda = new String[tamanhoDaAgenda];
+    private static String[] emailsDaAgenda = new String[tamanhoDaAgenda];
+    private static int poxicaoAtualAgenda = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.printf("Você deseja:%n1 - Adicionar novo contato%n2 - Pesquisar um contato%n3 - Imprimir todos os contatos%n4 - Imprimir contatos por letra do nome%n5 - Encerrar%n");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    adicionarContato();
+                    break;
+                case 2:
+                    pesquisarContato();
+                    break;
+                case 3:
+                    imprimirTodosContatos();
+                    break;
+                case 4:
+                    imprimirContatosPorletra();
+                    break;
+                case 5:
+                    System.out.println("Encerrando a agenda. Obrigado pelo tempo de uso :)");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 5);
+    }
+
+    private static void adicionarContato() {
+        if (poxicaoAtualAgenda < tamanhoDaAgenda) {
+            Scanner adicao = new Scanner(System.in);
+
+            boolean nomeValido = false;
+            do {
+                System.out.println("Digite o nome do contato (sem números):");
+                String nome = adicao.nextLine();
+                if (nome.matches(".*\\d.*")) {
+                    System.out.println("Nome inválido. Não deve conter números.");
+                } else {
+                    nomesDaAgenda[poxicaoAtualAgenda] = nome;
+                    nomeValido = true;
+                }
+            } while (!nomeValido);
+
+            boolean numeroValido = false;
+            do {
+                System.out.println("Digite o número do contato (apenas números):");
+                String numero = adicao.nextLine();
+                if (!numero.matches("\\d+")) {
+                    System.out.println("Número inválido. Deve conter apenas números.");
+                } else {
+                    numerosDaAgenda[poxicaoAtualAgenda] = numero;
+                    numeroValido = true;
+                }
+            } while (!numeroValido);
+
+            boolean emailValido = false;
+            do {
+                System.out.println("Digite o e-mail do contato (lembre-se do @):");
+                String email = adicao.nextLine();
+                if (!email.contains("@")) {
+                    System.out.println("E-mail inválido. Deve conter '@'.");
+                } else {
+                    emailsDaAgenda[poxicaoAtualAgenda] = email;
+                    emailValido = true;
+                }
+            } while (!emailValido);
+
+            poxicaoAtualAgenda++;
+
+            System.out.println("Contato adicionado com sucesso!");
+        } else {
+
+            System.out.println("A agenda está cheia. Não é possível adicionar mais contatos.");
+        }
+    }
+
+
+    private static void pesquisarContato() {
+        Scanner pesquisa = new Scanner(System.in);
+
+        System.out.println("Digite o nome do contato que deseja pesquisar:");
+        String nomePesquisado = pesquisa.nextLine();
+
+        boolean contatoEncontrado = false;
+
+        for (int i = 0; i < poxicaoAtualAgenda; i++) {
+            if (nomesDaAgenda[i].equalsIgnoreCase(nomePesquisado)) {
+                System.out.println("Contato encontrado:");
+                System.out.println("Nome: " + nomesDaAgenda[i]);
+                System.out.println("Número: " + numerosDaAgenda[i]);
+                System.out.println("E-mail: " + emailsDaAgenda[i]);
+                contatoEncontrado = true;
+                break;
+            }
+        }
+
+        if (!contatoEncontrado) {
+            System.out.println("Contato não encontrado.");
+        }
+    }
+
+    private static void imprimirTodosContatos() {
+        if (poxicaoAtualAgenda > 0) {
+            System.out.println("Todos os contatos cadastrados:");
+
+            for (int i = 0; i < poxicaoAtualAgenda; i++) {
+                System.out.println("Nome: " + nomesDaAgenda[i]);
+                System.out.println("Número: " + numerosDaAgenda[i]);
+                System.out.println("E-mail: " + emailsDaAgenda[i]);
+                System.out.println(" ");
+            }
+        } else {
+            System.out.println("Nenhum contato cadastrado.");
+        }
+    }
+
+    private static void imprimirContatosPorletra() {
+        Scanner imprimirContatos = new Scanner(System.in);
+        System.out.println("Digite a letra do nome:");
+
+        char letra = imprimirContatos.next().charAt(0);
+
+        System.out.println("Contatos cujo nome começa com '" + letra + "':");
+
+        boolean encontrouContato = false;
+
+        for (int i = 0; i < poxicaoAtualAgenda; i++) {
+            if (nomesDaAgenda[i].toLowerCase().charAt(0) == Character.toLowerCase(letra)) {
+                System.out.println("Nome: " + nomesDaAgenda[i]);
+                System.out.println("Número: " + numerosDaAgenda[i]);
+                System.out.println("E-mail: " + emailsDaAgenda[i]);
+                System.out.println(" ");
+                encontrouContato = true;
+            }
+        }
+
+        if (!encontrouContato) {
+            System.out.println("Nenhum contato encontrado com a letra '" + letra + "'.");
+        }
+    }
+}
